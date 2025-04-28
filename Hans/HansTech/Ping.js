bcd03f877d1fcea1b517052739e56fba7917c944
HANSTZ <hanstzmd@gmail.com>
2025-04-29 00:11:03 +0300

    Add files via upload

---
+++ Hans/HansTech/Ping.js
@@ -0,0 +1,15 @@
+import config from '../../config.cjs';
+const ping = async (m, sock) => {
+const prefix = config.PREFIX;
+const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+const text = m.body.slice(prefix.length + cmd.length).trim();
+if (cmd === "ping") {
+const start = new Date().getTime();
+await m.React('🚀');
+const end = new Date().getTime();
+const responseTime = (end - start) / 1000;
+const text = `🚀 Hans-Xmd_v3 speed is80${responseTime.toFixed(2)}8 ms*`;
+sock.sendMessage(m.from, { text }, { quoted: m });
+}
+}
+export default ping;
