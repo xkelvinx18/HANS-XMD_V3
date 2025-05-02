69956cbfe8c523f65762706722b2bc10a326d923
HANSTZ <hanstzmd@gmail.com>
2025-05-02 06:41:18 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -5,7 +5,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "HansTz&RHcjkBYR#xvN1uaxsMAVsOqF0zaSxe9PR2EwYi1vJKiFCfIIyHbI",
+  SESSION_ID: process.env.SESSION_ID || "",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
